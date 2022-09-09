f3d5fd4fae097d54c98cdc60f7b47b595f28375d
Yukimato <betatorium@gmail.com>
2022-09-09 14:39:54 +0300

    Improved RoomGen

--- RoomGen.cs
+++ RoomGen.cs
@@ -15,12 +15,30 @@ public class RoomGen : Node2D
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
-      var roomrng = new RandomNumberGenerator();
-      roomrng.Randomize();
-      int i = roomrng.RandiRange(0, 1);
-      rums = (PackedScene)GD.Load(list[i]);
-      TileMap rmSpawn = (TileMap)rums.Instance();
-      AddChild(rmSpawn);
+      for(int i = 0; i < 5; i++)
+      {
+        // Randomizer
+        var roomrng = new RandomNumberGenerator();
+        roomrng.Randomize();
+        int a = roomrng.RandiRange(0, 1);
+        rums = (PackedScene)GD.Load(list[a]);
+
+        TileMap rmSpawn = (TileMap)rums.Instance();
+        Vector2 rmPos = new Vector2(0, 600*i);
+        rmSpawn.Position = rmPos;
+          
+          for(int n = 0; n < 4; n++)
+          {
+            roomrng.Randomize();
+            a = roomrng.RandiRange(0, 1);
+            rums = (PackedScene)GD.Load(list[a]);
+
+            rmSpawn = (TileMap)rums.Instance();
+            rmPos.x = n * 1024;
+            rmSpawn.Position = rmPos;
+            AddChild(rmSpawn);
+          }
+      }
     }
 
 //  // Called every frame. 'delta' is the elapsed time since the previous frame.
